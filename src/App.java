b25eb3237483ca6ef2822a308ff582620d152221
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-09-08 11:38:41 +0200

    commit

--- src/App.java
+++ src/App.java
@@ -1,9 +1,16 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
+    public static double plus(int tal1, int tal2) {
+
+        return (double) tal1 + tal2;
+    }
+
     public static void main(String[] args) throws Exception {
+        
         Scanner scanner = new Scanner(System.in);
-        int val = scanner.nextInt();
+        val = scanner.nextInt();
         boolean bool;
         bool = true;
 
@@ -15,7 +22,24 @@ public class App {
             System.out.println("4.division");
             System.out.println("5.Avsluta");
             System.out.println("Välj 1-5: ");
+
+
         }
 
+        if (val == 5){
+            System.out.println("Stänger av programmet.");
+            bool = false;
+        }
+
+        // else if (val  == 1){
+        // System.out.println("Ange tal 1");
+        // double num1 = scanner.nextDouble();
+
+        // System.out.println("Ange tal 2");
+        // double num2 = scanner.nextDouble();
+
+        // double result = 0;
+        // }
+        scanner.close();
     }
 }
