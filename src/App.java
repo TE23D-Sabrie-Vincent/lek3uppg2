1fc54d9fb5c221362eef15d850cc24bbc5efa68b
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-09-08 15:20:13 +0200

    Klart

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+
+// import java.util.InputMismatchException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,14 +9,28 @@ public class App {
         return (double) tal1 + tal2;
     }
 
-    public static void main(String[] args) throws Exception {
-        
-        Scanner scanner = new Scanner(System.in);
-        val = scanner.nextInt();
-        boolean bool;
-        bool = true;
+    public static double minus(int a, int b) {
+        return (double) a - b;
+    }
 
-        while (bool) {
+    public static double multiply(int a, int b) {
+        return (double) a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division med noll är inte tillåten!");
+        }
+        return (double) a / (double) b;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running;
+        running = true;
+
+        while (running) {
             System.out.println("Meny");
             System.out.println("1.Addition");
             System.out.println("2.Subtraktion");
@@ -23,23 +39,53 @@ public class App {
             System.out.println("5.Avsluta");
             System.out.println("Välj 1-5: ");
 
+            int val = 0;
 
+            try {
+                val = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Fel: skriv endast nummer 1-5!");
+                scanner.next(); // rensar felaktig inmatning
+                continue;
+            }
+
+            if (val == 5) {
+                System.out.println("Stänger av programmet.");
+                running = false;
+            }
+
+            else if (val >= 1 && val <= 4) {
+                try{
+               System.out.println("Skriv tal 1");
+                    int tal1 = scanner.nextInt();
+
+                    System.out.println("Ange tal 2: ");
+                    int tal2 = scanner.nextInt();
+
+                    double result = 0;
+                    
+                    switch (val) {
+                        case 1: result = plus(tal1, tal2); break;
+                        case 2: result = minus(tal1, tal2); break;
+                        case 3: result = multiply(tal1, tal2); break;
+                        case 4: result = divide(tal1, tal2); break;
+                    }
+                    System.out.println("Resultat: " + result);
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Fel: du måste skriva in heltal!");
+                    scanner.next();
+                }
+                catch (ArithmeticException e){
+                    System.out.println("Fel: " + e.getMessage());
+
+                }
+            }
+            else{
+                System.out.println("Ogiltigt val, välj 1-5!");
+                }
+                System.out.println();
         }
-
-        if (val == 5){
-            System.out.println("Stänger av programmet.");
-            bool = false;
-        }
-
-        // else if (val  == 1){
-        // System.out.println("Ange tal 1");
-        // double num1 = scanner.nextDouble();
-
-        // System.out.println("Ange tal 2");
-        // double num2 = scanner.nextDouble();
-
-        // double result = 0;
-        // }
         scanner.close();
     }
 }
